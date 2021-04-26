259dd3e053f064dd7bf5d3253e7f7fa409972cb2
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2021-04-26 09:17:52 +0530

    filter and add expense feature added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,7 @@ import './Expenses.css';
 
 import Card from './../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState(new Date().getFullYear().toString());
@@ -13,14 +13,15 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+        <ExpensesList items={filteredExpenses}></ExpensesList>
       </Card>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,32 @@
+import { useState } from 'react';
+
 import './NewExpense.css';
 
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    
+
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const toggleEditingHandler = () => {
+    setIsEditing((prevState) => {
+      return !prevState;
+    });
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
+      {!isEditing && <button onClick={toggleEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onCancel={toggleEditingHandler} onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>}
     </div>
   );
 };
